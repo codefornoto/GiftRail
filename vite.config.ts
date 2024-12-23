0993a7e2517191f2896c224d5f60740f2723ee68
kenshgy <ken.shgy.1386@gmail.com>
2024-12-23 14:44:29 +0900

    fix bugs

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/GiftRoute',
+  // base: '/GiftRoute',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
